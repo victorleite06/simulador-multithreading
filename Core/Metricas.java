69b92834e4cb64fae358e481ee0ae3d75ecf4187
Victor Monteiro <vgrataroli@sga.pucminas.br>
2024-12-11 23:15:04 -0300

    Update Metricas.java

--- Core/Metricas.java
+++ Core/Metricas.java
@@ -2,16 +2,18 @@ package Core;
 
 import java.util.List;
 
+import Arquiteturas.ArquiteturaBase;
+
 public class Metricas {
     public static double calcularIPC(int instrucoesExecutadas, int ciclos) { // Cálculo do IPC (Instruções por ciclo)
         return (double) instrucoesExecutadas / ciclos;
     }
 
-    public static double calcularCustoPorInstrucao(List<Instrucao> instrucoes, int ciclos) { // Cálculo do custo por instrução
-        if (instrucoes.isEmpty()){
+    public static double calcularCustoPorInstrucao(int instrucoesExecutadas, int ciclos) { // Cálculo do custo por instrução
+        if (instrucoesExecutadas == 0){
             return 0.0;
         }
-        return (double) ciclos / instrucoes.size();
+        return (double) ciclos / instrucoesExecutadas;
     }
 
     public static double calcularCustoBolhas(int bolhas, int ciclos) { // Cálculo do custo de bolhas
@@ -20,4 +22,5 @@ public class Metricas {
         }
         return (double) bolhas / ciclos * 100; // Percentual de bolhas
     }
+
 }
