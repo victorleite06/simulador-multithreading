635e4c01f4b9217cc85f77f18f38ce9356256091
Victor Monteiro <vgrataroli@sga.pucminas.br>
2024-12-11 23:13:16 -0300

    Update BMT.java

--- Arquiteturas/BMT.java
+++ Arquiteturas/BMT.java
@@ -16,36 +16,36 @@ public class BMT extends ArquiteturaBase {
     private int pipelineEX = -1; // Execute
     private int pipelineWB = -1; // Write Back
 
+    private Instrucao[] pipeline;
+    private int tamanhoPipeline = 4;
+
     // Construtores
     public BMT(int numeroRegistradores) {
         super(numeroRegistradores, 1024); // Inicializa com memória de tamanho 1024
+        pipeline = new Instrucao[tamanhoPipeline];
     }
 
     // Sobrescreve o método de execução do ciclo (Lógica de execução do BMT)
     @Override
     protected void executarCiclo() {
-        if (this.instrucoes.isEmpty()) { // Lista de instruções vazia
+        if (this.instrucoes.isEmpty() && pipeline[tamanhoPipeline - 1] == null) { // Lista de instruções vazia
             pararExecucao();
             return;
         }
 
-        // Em um ciclo, até 4 instruções podem ser processadas
-        int maxInstrucoesPorCiclo = 4;
-
-        // Processa até o limite de instruções por ciclo
-        for (int i = 0; i < maxInstrucoesPorCiclo && !this.instrucoes.isEmpty(); i++) {
-            Instrucao instrucao = this.instrucoes.remove(0);
-            System.out.println("Executando instrução " + instrucao.getTipo() + " no BMT");
-
-            // Simula o pipeline
-            fetch(instrucao);
-            decode();
-            execute();
-            writeBack();
+        //Avança o pipeline
+        for (int i = tamanhoPipeline-1; i > 0; i--) {
+            pipeline[i] = pipeline[i-1];
         }
+        pipeline[0] = this.instrucoes.isEmpty() ? null : this.instrucoes.remove(0);
 
-        // Incrementa o ciclo de execução
+        if(pipeline[tamanhoPipeline-1] != null){
+            System.out.println("Executando instrução " + pipeline[tamanhoPipeline-1].getTipo() + " no BMT");
+            executarInstrucoes(pipeline[tamanhoPipeline-1]);
+            instrucoesExecutadas++;
+        }
         incrementarCiclo();
+        
     }
 
     // Simula a fase de busca da instrução (Instruction Fetch)
